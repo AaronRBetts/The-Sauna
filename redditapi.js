2e266666434c153f20cd6a99aad73b55221b216d
AaronRBetts <aaronrbetts@gmail.com>
2020-05-09 10:48:40 +0300

    experimenting with build package

--- redditapi.js
+++ redditapi.js
@@ -14,7 +14,7 @@ export default {
             searchLimit = `&limit=${searchLimit}`;      
         }
         return fetch(
-            `http://www.reddit.com/r/finland/${searchTerm}${sortBy}${searchLimit}&restrict_sr=on`
+            `https://www.reddit.com/r/finland/${searchTerm}${sortBy}${searchLimit}&restrict_sr=on`
         )
         .then(res => res.json())
         .then(data => data.data.children.map(data => data.data))
