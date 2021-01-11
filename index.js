24fbe7746bbc5d209b3537fc405a3aed1e6fb99d
AaronRBetts <aaronrbetts@gmail.com>
2021-01-11 17:18:11 +0200

    maintenance on reddit search function

--- index.js
+++ index.js
@@ -5,46 +5,60 @@ const searchInput = document.getElementById("search-input");
 
 searchReddit("", 25, "hottest");
 
-searchForm.addEventListener('submit', e => {
-    e.preventDefault();
-    //Get the search term
-    const searchTerm = searchInput.value;
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //Get the search term
+  const searchTerm = searchInput.value;
 
-    const sortBy = document.querySelector('input[name="sortby"]:checked').value;
+  const sortBy = document.querySelector('input[name="sortby"]:checked').value;
 
-    // if (searchTerm === "") {
-    //     showMessage('Please add a search term', 'alert-danger');
-    // }
+  // if (searchTerm === "") {
+  //     showMessage('Please add a search term', 'alert-danger');
+  // }
 
-    //Clear input
-    searchReddit("", 25, "");
-    searchInput.value = "";
+  //Clear input
+  searchReddit(searchTerm, 25, sortBy);
+  searchInput.value = "";
 });
 
-function searchReddit(searchTerm, searchLimit, sortBy){
+function searchReddit(searchTerm, searchLimit, sortBy) {
+  console.log(`searching ${searchTerm}`)
+  // Search Reddit
+  reddit.search(searchTerm, searchLimit, sortBy).then((results) => {
+    let output = '<div class="card-columns">';
+    results.forEach((post) => {
+      if (post.domain == "self.Finland") post.domain = `reddit.com/r/finland`;
+      //Check for image
+      let image = ``;
+      if (post.preview) {
+        image = `<a href="${post.url}" target="_blank"><img src="${post.preview.images[0].source.url}" class="card-img-top img-fluid" alt="article image"></a>`;
+      }
+      var a = new Date(post.created_utc * 1000);
+      var months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ];
+      var year = a.getFullYear();
+      var month = months[a.getMonth()];
+      var date = a.getDate();
+      var hour = a.getHours();
+      var min = a.getMinutes();
+      var sec = a.getSeconds();
+      var time =
+        date + " " + month + " " + year + " " + hour + ":" + min + ":" + sec;
 
-    // Search Reddit
-    reddit.search(searchTerm, searchLimit, sortBy).then(results => {
-        let output = '<div class="card-columns">';
-        results.forEach(post => {
-            if (post.domain == "self.Finland") post.domain = `reddit.com/r/finland`
-            //Check for image
-            let image = ``;
-            if (post.preview){
-                image = `<a href="${post.url}" target="_blank"><img src="${post.preview.images[0].source.url}" class="card-img-top img-fluid" alt="article image"></a>`
-            } 
-            var a = new Date(post.created_utc * 1000);
-            var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-            var year = a.getFullYear();
-            var month = months[a.getMonth()];
-            var date = a.getDate();
-            var hour = a.getHours();
-            var min = a.getMinutes();
-            var sec = a.getSeconds();
-            var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
-
-            //Produce card with info
-            output += `
+      //Produce card with info
+      output += `
             <div class="card text-white bg-dark">
                 <div class="card-body">
                     ${image}
@@ -52,38 +66,48 @@ function searchReddit(searchTerm, searchLimit, sortBy){
                     <span class="badge badge-secondary">Posted: ${time}</span>
                     <hr>
                     <p class="card-text">${truncateText(post.selftext, 100)}</p>
-                    <a href="${post.url}" target="_blank" class="btn btn-primary">Read more</a>
-                    <span class="badge badge-secondary">Article origin: ${post.domain}</span>
+                    <a href="${
+                      post.url
+                    }" target="_blank" class="btn btn-primary">Read more</a>
+                    <span class="badge badge-secondary">Article origin: ${
+                      post.domain
+                    }</span>
                 </div>
             </div>
             `;
-        })
-        output += '</div>';
-        document.getElementById('results').innerHTML = output;
     });
+    output += "</div>";
+    document.getElementById("results").innerHTML = output;
+  });
 }
-    
 
-function showMessage(message, className){
-    //create message div
-    const div = document.createElement('div');
-    div.className = `alert ${className}`;
-    div.appendChild(document.createTextNode(message));
+function showMessage(message, className) {
+  //create message div
+  const div = document.createElement("div");
+  div.className = `alert ${className}`;
+  div.appendChild(document.createTextNode(message));
 
-    //get parent (search container)
-    const searchContainer = document.getElementById("search-container");
-    const search = document.getElementById("search");
+  //get parent (search container)
+  const searchContainer = document.getElementById("search-container");
+  const search = document.getElementById("search");
 
-    //insert a message to the parent
-    searchContainer.insertBefore(div, search);
+  //insert a message to the parent
+  searchContainer.insertBefore(div, search);
 
-    //Timeout alert
-    setTimeout(() => document.querySelector(".alert").remove(), 3000);
+  //Timeout alert
+  setTimeout(() => document.querySelector(".alert").remove(), 3000);
 }
 
 //Truncate text
 function truncateText(text, limit) {
-    const shortened = text.indexOf(' ', limit);
-    if (shortened == -1) return text;
-    return text.substring(0, shortened);
+  const shortened = text.indexOf(" ", limit);
+  if (shortened == -1) return text;
+  return text.substring(0, shortened);
 }
+
+document.getElementById("loadBG").onclick = function () {
+  let randomInt = Math.floor(Math.random() * (100 - 1) + 1);
+  document.getElementById(
+    "main"
+  ).style.backgroundImage = `url(https://source.unsplash.com/1920x1080/?finland?sig=${randomInt})`;
+};
